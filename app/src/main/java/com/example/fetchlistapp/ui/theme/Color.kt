3161ef14cbee3f307309b7c4eaa72de1cb3fa785
Shenabeth Jenkins <shenabeth@gmail.com>
2025-05-02 16:34:13 -0400

    Update Color.kt

--- app/src/main/java/com/example/fetchlistapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/fetchlistapp/ui/theme/Color.kt
@@ -2,15 +2,6 @@ package com.example.fetchlistapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-// Default color palette
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
-
 // Custom color palette
 val PurpleDark = Color(0xFF300B3F)
 val GoldDark = Color(0xFFF3A412)
@@ -19,5 +10,4 @@ val White = Color(0xFFFFFFFF)
 // Additional colors for the theme
 val PurpleLight = Color(0xFF5E336F)
 val GoldLight = Color(0xFFF6BC4D)
-val Grey = Color(0xFFAAAAAA)
 val Black = Color(0xFF000000)
